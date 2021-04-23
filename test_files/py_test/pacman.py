552b0eb4a9b4a9ed20f21643f821b7f4056703f2
Natalie Orsi <norsi3@gatech.edu>
2021-04-23 10:43:50 -0700

    Update pacman.py
    Simplifying Request to first just receive the input rather than parse as well (unlike previous version), and will pass along to processing functions

--- test_files/py_test/pacman.py
+++ test_files/py_test/pacman.py
@@ -5,6 +5,11 @@ Write a module docstring here
 __author__ = "Your Name"
 
 
+from collections import namedtuple
+
+Coords = namedtuple("Coords", ("x","y"))
+
+
 def pacman(input_file):
     """ Use this function to format your input/output arguments. Be sure not to change the order of the output arguments. 
     Remember that code organization is very important to us, so we encourage the use of helper fuctions and classes as you see fit.
@@ -17,14 +22,22 @@ def pacman(input_file):
         2. final_pos_y (int) = final y location of Pacman
         3. coins_collected (int) = the number of coins that have been collected by Pacman across all movements
     """
-    return 0,0,0
+    r = Request(input_file)
+    return r._raw_req
     # return final_pos_x, final_pos_y, coins_collected 
 
 class Request:
-    _board_dimension = None
-    _initial_position = None
-    _movements = None
-    _walls = None
+    _raw_req = None
+    _final_req = None
     
-    def __init__():
+    def __init__(self, file_name):
+        if file_name:
+            self._raw_req = self.filerequest(file_name)
         return
+    
+    @staticmethod
+    def filerequest(f):
+        with open(f, "r") as f:
+             #ignore anything after 1000 bytes as out of scope
+             return f.readlines(1000)
+                
